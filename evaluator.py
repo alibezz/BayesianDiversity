634fb03585cfbd408303313058af89600d0b37ef
alibezz <alibezz@gmail.com>
2012-07-04 17:05:03 -0300

    adding evaluation schema

---
+++ evaluator.py
@@ -0,0 +1,12 @@
+'''
+Implementation of different 
+evaluation metrics for top-n
+'''
+
+class Evaluator(object):
+
+    def __init__(self, users):
+        self.users = users
+
+    def totalOfRatings(self, predictions):
+        pass
