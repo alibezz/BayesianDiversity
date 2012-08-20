4e9d5eb22bfdc8ff19295ad3af47dc99b47437aa
alibezz <alibezz@gmail.com>
2012-08-20 16:29:19 -0300

    generating a new way of splitting data, so that my experiments can be compatible with Cremonesi's

--- generate_validation_folds.py
+++ generate_validation_folds.py
@@ -56,13 +56,52 @@ class GenerateValidationFolds(object):
 
         ids = [ind for ind, obj in enumerate(folds)]
         for ind, obj in enumerate(folds):
-            #change the way you write for binary w/ compression
+            #TODO change the way you write for binary w/ compression
             f = open(folder + "/test" + str(ind), "w")
             self.__write_folds([ind], f, folds)
             f.close()
             f = open(folder + "/training" + str(ind), "w") 
             self.__write_folds(list(set(ids) - set([ind])), f, folds)
             f.close()
+
+    def generate_probe_set(self, instances, percentage):
+        '''
+        This is a probe set to follow Cremonesi's evaluation
+        methodology
+        '''
+        import random
+        
+        probe_size = len(instances) * percentage
+        return random.sample([i for i in xrange(len(instances))], int(probe_size))
+
+    def write_cremonesi_validation(self, instances, probe_ids):
+        '''
+        writing training and test sets according to Cremonesi's validation
+        methodology
+        '''
+        test_set = []
+        for i in probe_ids:
+            inst = self.__format_example(instances[i])
+            if inst.split('\\t')[2] == '5':
+                test_set += instances[i]
+
+        training_set = []
+        for i in xrange(len(instances)):
+            if i not in probe_ids:
+                training_set += instances[i]
+
+        #TODO remove redundancy
+        folder = self.__create_validation_folder()
+        f = open(folder + "/training", "w")
+        for inst in training_set:
+            f.write(self.__format_example(inst))
+        f.close()
+
+        f = open(folder + "/test", "w")
+        for inst in test_set:
+            f.write(self.__format_example(inst))
+        f.close()
+
 
 if __name__=="__main__":
     '''
@@ -75,4 +114,8 @@ if __name__=="__main__":
     m.read_lines()
     insts = m.extract_data_from_lines(sys.argv[2]) #~400MB
     a = GenerateValidationFolds(insts, int(sys.argv[3]))
-    a.write_cross_validation_data(a.generate_folds())
+    probe = a.generate_probe_set(insts, 0.014)
+    a.write_cremonesi_validation(insts, probe)
+    
+
+    #a.write_cross_validation_data(a.generate_folds())
